b224f15032c1ee58757b138ecf5daf0f4dd59c3f
Raj <149650780+raj11gautam@users.noreply.github.com>
2025-08-19 16:31:46 +0530

    Create Sum.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+package classWork;
+import java.util.Scanner;
+
+public class BridgeLabz2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int Sum = a+b;
+        System.out.println(Sum);
+    }
+}
